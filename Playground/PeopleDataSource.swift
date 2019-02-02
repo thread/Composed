4aafa36cd2ded6845f597b2e648d36bf2987f640
Shaps Benkau <shapsuk@me.com>
2019-02-02 01:50:22 +0000

    Fixed didBecomeActive in AggregateDataSource Updated PeopleDataSource

--- Playground/PeopleDataSource.swift
+++ Playground/PeopleDataSource.swift
@@ -11,6 +11,32 @@ final class PersonCell: DataSourceCell, ReusableViewNibLoadable {
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var ageLabel: UILabel!
 
+    override var isHighlighted: Bool {
+        didSet { layer.add(CATransition(), forKey: nil) }
+    }
+
+    override var isSelected: Bool {
+        didSet { layer.add(CATransition(), forKey: nil) }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        backgroundView = UIView(frame: .zero)
+        backgroundView?.layer.cornerRadius = 6
+        backgroundView?.layer.borderWidth = 1
+        backgroundView?.layer.borderColor = UIColor.lightGray.cgColor
+        backgroundView?.backgroundColor = UIColor(white: 0.98, alpha: 1)
+
+        selectedBackgroundView = UIView(frame: .zero)
+        selectedBackgroundView?.layer.cornerRadius = 6
+        selectedBackgroundView?.layer.borderWidth = 1
+        selectedBackgroundView?.layer.borderColor = UIColor.lightGray.cgColor
+        selectedBackgroundView?.backgroundColor = UIColor(white: 0.88, alpha: 1)
+
+        backgroundColor = .clear
+    }
+
     public func prepare(person: Person) {
         nameLabel.text = person.name
         ageLabel.text = "\(person.age)"
@@ -36,7 +62,7 @@ final class PeopleDataSource: SimpleDataSource<Person> {
     private let prototypeHeader = HeaderView.fromNib
 
     override func metrics(for section: Int) -> DataSourceSectionMetrics {
-        return DataSourceSectionMetrics(columnCount: 2, insets: UIEdgeInsets(horizontal: 16, vertical: 2), horizontalSpacing: 4, verticalSpacing: 4)
+        return DataSourceSectionMetrics(columnCount: 2, insets: UIEdgeInsets(all: 16), horizontalSpacing: 4, verticalSpacing: 4)
     }
 
     override func cellConfiguration(for indexPath: IndexPath) -> CellConfiguration {
