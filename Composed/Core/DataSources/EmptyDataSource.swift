8ddce7c132095bca670064a73ef980a48de9e4ae
Shaps Benkau <shapsuk@me.com>
2019-05-07 11:54:27 +0100

    Committing in-progress work to be able to merge in changes from master

---
+++ Composed/Core/DataSources/EmptyDataSource.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+//open class EmptyDataSource: DataSource {
+//
+//    public var isEmpty: Bool { return true }
+//    public weak var updateDelegate: DataSourceUpdateDelegate?
+//    public var numberOfSections: Int { return 1 }
+//    public func numberOfElements(in section: Int) -> Int { return 0 }
+//
+//}
