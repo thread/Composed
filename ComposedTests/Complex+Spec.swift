7a4c9c32659c1c331cb64d09534631f5bbb3db47
Shaps Benkau <shapsuk@me.com>
2019-05-08 19:52:32 +0100

    Added a lot more tests, 52 in total

--- ComposedTests/Complex+Spec.swift
+++ ComposedTests/Complex+Spec.swift
@@ -11,6 +11,8 @@ final class Complex_Spec: QuickSpec {
         var list1: ArrayDataSource<Int>!
         var list2: ArrayDataSource<Int>!
         var list3: ArrayDataSource<Int>!
+        var empty1: EmptyDataSource!
+        var empty2: EmptyDataSource!
         var sectioned: SectionedDataSource<Int>!
         var countries: SectionedDataSource<String>!
         var segmented: SegmentedDataSource!
@@ -29,22 +31,43 @@ final class Complex_Spec: QuickSpec {
                 list2.store.elements,
             ])
 
+            empty1 = EmptyDataSource()
+            empty2 = EmptyDataSource()
+
             countries = SectionedDataSource(elements: countryNames)
-            segmented = SegmentedDataSource(children: [EmptyDataSource(), list1])
+            segmented = SegmentedDataSource(children: [empty1, list1])
             innerComposed = ComposedDataSource(children: [segmented, list3])
-            outerComposed = ComposedDataSource(children: [sectioned, innerComposed, countries, EmptyDataSource()])
+            outerComposed = ComposedDataSource(children: [sectioned, innerComposed, countries, empty2])
         }
 
-        describe("Given a complex dataSource heirachy") {
-            it("the dataSource's should have the expected counts") {
-                expect(list1.numberOfSections).to(equal(1))
-                expect(list2.numberOfSections).to(equal(1))
-                expect(list3.numberOfSections).to(equal(1))
-                expect(sectioned.numberOfSections).to(equal(2))
-                expect(countries.numberOfSections).to(equal(1))
-                expect(segmented.numberOfSections).to(equal(1))
-                expect(innerComposed.numberOfSections).to(equal(2))
-                expect(outerComposed.numberOfSections).to(equal(6))
+        describe("Given a dataSource heirachy") {
+            context("when no changes have occurred") {
+                it("section counts should be as expected") {
+                    expect(list1.numberOfSections).to(equal(1))
+                    expect(list2.numberOfSections).to(equal(1))
+                    expect(list3.numberOfSections).to(equal(1))
+                    expect(sectioned.numberOfSections).to(equal(2))
+                    expect(countries.numberOfSections).to(equal(1))
+                    expect(segmented.numberOfSections).to(equal(1))
+                    expect(innerComposed.numberOfSections).to(equal(2))
+                    expect(outerComposed.numberOfSections).to(equal(6))
+                }
+
+                it("local section data should be valid") {
+                    let actual0 = outerComposed.localSection(for: 0)
+                    let actual1 = outerComposed.localSection(for: 1)
+                    let actual2 = outerComposed.localSection(for: 2)
+                    let actual3 = outerComposed.localSection(for: 3)
+                    let actual4 = outerComposed.localSection(for: 4)
+                    let actual5 = outerComposed.localSection(for: 5)
+
+                    expect(DataSourceHashableWrapper(actual0.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual1.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual2.dataSource)).to(equal(DataSourceHashableWrapper(empty1)))
+                    expect(DataSourceHashableWrapper(actual3.dataSource)).to(equal(DataSourceHashableWrapper(list3)))
+                    expect(DataSourceHashableWrapper(actual4.dataSource)).to(equal(DataSourceHashableWrapper(countries)))
+                    expect(DataSourceHashableWrapper(actual5.dataSource)).to(equal(DataSourceHashableWrapper(empty2)))
+                }
             }
 
             context("when segmented.selectedChild == nil") {
@@ -52,10 +75,24 @@ final class Complex_Spec: QuickSpec {
                     segmented.setSelected(index: nil)
                 }
 
-                it("the dataSource counts should be adjusted") {
+                it("section counts should be as expected") {
                     expect(segmented.numberOfSections).to(equal(0))
                     expect(innerComposed.numberOfSections).to(equal(1))
                     expect(outerComposed.numberOfSections).to(equal(5))
+                }
+
+                it("local section data should be valid") {
+                    let actual0 = outerComposed.localSection(for: 0)
+                    let actual1 = outerComposed.localSection(for: 1)
+                    let actual2 = outerComposed.localSection(for: 2)
+                    let actual3 = outerComposed.localSection(for: 3)
+                    let actual4 = outerComposed.localSection(for: 4)
+
+                    expect(DataSourceHashableWrapper(actual0.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual1.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual2.dataSource)).to(equal(DataSourceHashableWrapper(list3)))
+                    expect(DataSourceHashableWrapper(actual3.dataSource)).to(equal(DataSourceHashableWrapper(countries)))
+                    expect(DataSourceHashableWrapper(actual4.dataSource)).to(equal(DataSourceHashableWrapper(empty2)))
                 }
             }
 
@@ -64,9 +101,23 @@ final class Complex_Spec: QuickSpec {
                     innerComposed.remove(dataSource: segmented)
                 }
 
-                it("the dataSource counts should be adjusted") {
+                it("section counts should be as expected") {
                     expect(innerComposed.numberOfSections).to(equal(1))
                     expect(outerComposed.numberOfSections).to(equal(5))
+                }
+
+                it("local section data should be valid") {
+                    let actual0 = outerComposed.localSection(for: 0)
+                    let actual1 = outerComposed.localSection(for: 1)
+                    let actual2 = outerComposed.localSection(for: 2)
+                    let actual3 = outerComposed.localSection(for: 3)
+                    let actual4 = outerComposed.localSection(for: 4)
+
+                    expect(DataSourceHashableWrapper(actual0.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual1.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual2.dataSource)).to(equal(DataSourceHashableWrapper(list3)))
+                    expect(DataSourceHashableWrapper(actual3.dataSource)).to(equal(DataSourceHashableWrapper(countries)))
+                    expect(DataSourceHashableWrapper(actual4.dataSource)).to(equal(DataSourceHashableWrapper(empty2)))
                 }
             }
 
@@ -75,15 +126,30 @@ final class Complex_Spec: QuickSpec {
                     innerComposed.removeAll()
                 }
 
-                it("the dataSource counts should be adjusted") {
+                it("section counts should be as expected") {
                     expect(innerComposed.numberOfSections).to(equal(0))
                     expect(outerComposed.numberOfSections).to(equal(4))
+                }
+
+                it("local section data should be valid") {
+                    let actual0 = outerComposed.localSection(for: 0)
+                    let actual1 = outerComposed.localSection(for: 1)
+                    let actual2 = outerComposed.localSection(for: 2)
+                    let actual3 = outerComposed.localSection(for: 3)
+
+                    expect(DataSourceHashableWrapper(actual0.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual1.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual2.dataSource)).to(equal(DataSourceHashableWrapper(countries)))
+                    expect(DataSourceHashableWrapper(actual3.dataSource)).to(equal(DataSourceHashableWrapper(empty2)))
                 }
             }
 
             context("when all children are removed from outerComposed") {
-                it("the dataSource counts should be adjusted") {
+                beforeEach {
                     outerComposed.removeAll()
+                }
+
+                it("section counts should be as expected") {
                     expect(outerComposed.numberOfSections).to(equal(0))
                 }
             }
@@ -94,23 +160,57 @@ final class Complex_Spec: QuickSpec {
                     innerComposed.insert(dataSource: newList, at: 0)
                 }
 
-                it("the dataSource counts should be adjusted") {
+                it("section counts should be as expected") {
                     expect(innerComposed.numberOfSections).to(equal(3))
                     expect(outerComposed.numberOfSections).to(equal(7))
                 }
+
+                it("local section data should be valid") {
+                    let actual0 = outerComposed.localSection(for: 0)
+                    let actual1 = outerComposed.localSection(for: 1)
+                    let actual2 = outerComposed.localSection(for: 2)
+                    let actual3 = outerComposed.localSection(for: 3)
+                    let actual4 = outerComposed.localSection(for: 4)
+                    let actual5 = outerComposed.localSection(for: 5)
+                    let actual6 = outerComposed.localSection(for: 6)
+
+                    expect(DataSourceHashableWrapper(actual0.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual1.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual2.dataSource)).to(equal(DataSourceHashableWrapper(newList)))
+                    expect(DataSourceHashableWrapper(actual3.dataSource)).to(equal(DataSourceHashableWrapper(empty1)))
+                    expect(DataSourceHashableWrapper(actual4.dataSource)).to(equal(DataSourceHashableWrapper(list3)))
+                    expect(DataSourceHashableWrapper(actual5.dataSource)).to(equal(DataSourceHashableWrapper(countries)))
+                    expect(DataSourceHashableWrapper(actual6.dataSource)).to(equal(DataSourceHashableWrapper(empty2)))
+                }
             }
 
-            context("when a new child replaces an existing one in outerComposed") {
+            context("when a new child is inserted at 0 and the last element is removed from outerComposed") {
                 beforeEach {
                     newList = ArrayDataSource(elements: [0])
                     var children = outerComposed.children
                     children.removeLast()
-                    children.append(newList)
+                    children.insert(newList, at: 0)
                     outerComposed.replace(children, animated: false)
                 }
 
-                it("the dataSource counts should be adjusted") {
+                it("section counts should be as expected") {
                     expect(outerComposed.numberOfSections).to(equal(6))
+                }
+
+                it("local section data should be valid") {
+                    let actual0 = outerComposed.localSection(for: 0)
+                    let actual1 = outerComposed.localSection(for: 1)
+                    let actual2 = outerComposed.localSection(for: 2)
+                    let actual3 = outerComposed.localSection(for: 3)
+                    let actual4 = outerComposed.localSection(for: 4)
+                    let actual5 = outerComposed.localSection(for: 5)
+
+                    expect(DataSourceHashableWrapper(actual0.dataSource)).to(equal(DataSourceHashableWrapper(newList)))
+                    expect(DataSourceHashableWrapper(actual1.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual2.dataSource)).to(equal(DataSourceHashableWrapper(sectioned)))
+                    expect(DataSourceHashableWrapper(actual3.dataSource)).to(equal(DataSourceHashableWrapper(empty1)))
+                    expect(DataSourceHashableWrapper(actual4.dataSource)).to(equal(DataSourceHashableWrapper(list3)))
+                    expect(DataSourceHashableWrapper(actual5.dataSource)).to(equal(DataSourceHashableWrapper(countries)))
                 }
             }
         }
