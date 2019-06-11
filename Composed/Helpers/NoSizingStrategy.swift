8b8cbc74387ab1c4e66523e6fb14b14e0204bca3
Shaps Benkau <shapsuk@me.com>
2019-06-11 17:09:12 +0100

    Fixed a bug in NoSizingStrategy

--- Composed/Helpers/NoSizingStrategy.swift
+++ Composed/Helpers/NoSizingStrategy.swift
@@ -5,7 +5,7 @@ public struct NoSizingStrategy: CollectionUISizingStrategy {
     public init() { }
 
     public func cachedSize(forElementAt indexPath: IndexPath) -> CGSize? {
-        return nil
+        return .zero
     }
 
     public func size(forElementAt indexPath: IndexPath, context: CollectionUISizingContext, dataSource: DataSource) -> CGSize {
