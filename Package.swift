75b9500d2649f65b20ce89df057308056607ef8a
Joseph Duffy <commits@josephduffy.co.uk>
2019-10-29 12:29:44 +0000

    Pin Quick and Nimble versions

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/shaps80/FlowLayout.git", .exact("1.0.7")),
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.2"),
+        .package(url: "https://github.com/Quick/Quick.git", .exact("2.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .exact("8.0.2")),
     ],
     targets: [
         .target(
